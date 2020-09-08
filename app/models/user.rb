157790d642b6d14bf80bec2283ffe2d3bce90b79
tacodtripe <55043949+tacodtripe@users.noreply.github.com>
2020-09-08 04:11:42 -0600

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+    has_many :posts
+    has_many :comments
+
+    validates :username, :email, :password, presence: true
+
+    validates :username,
+    length: {minimum: 3, maximum: 20},
+    uniqueness: true
+    
+    validates :email,
+    format: {with: /[\w]+@[a-z]+\.[a-z]{1,63}/},
+    uniqueness: true
+
+    validates :password,
+    length: {minimum: 6, maximum: 20}
 end
